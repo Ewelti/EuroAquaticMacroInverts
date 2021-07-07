bc3e625997e755142b3c99c659b7445709102608
Diana Bowler <diana.bowler@idiv.de>
2021-07-07 18:01:17 +0200

    examine amount of data for each month

--- exploratory_plots.R
+++ exploratory_plots.R
@@ -9,6 +9,12 @@ library(ggthemes)
 DATA1_list <- read.csv("raw-data/All_indices_benthicMacroInverts_AllYears.csv", header=T) # change file name according to the time series to be analyzed
 DATA2 <- DATA1_list[!is.na(DATA1_list$site_id_wMissing),]
 
+#### cleaning ####
+
+unique(DATA2$month)
+DATA2$month[which(DATA2$month == "6to8")] <- "7"
+DATA2$month_numeric <- as.numeric(DATA2$month)
+
 #### quick exploratory analysis ####
 
 glimpse(DATA2)
@@ -24,8 +30,6 @@ DATA2 %>%
 DATA2 %>%
   group_by(country,site_id,year,sample_id) %>%
   count()
-
-table(DATA2$month)
 
 #### time-series plots ####
 
@@ -60,10 +64,26 @@ ggplot(DATA2, aes(x=year, y=spp_richness))+
 
 #### monthly pattern ?? ####
 
-unique(DATA2$month)
-DATA2$month[which(DATA2$month == "6to8")] <- "7"
-DATA2$month_numeric <- as.numeric(DATA2$month)
+#amount of data per month
+table(DATA2$month)
 
+monthlyData <- DATA2 %>%
+  group_by(country,month_numeric) %>%
+  summarize(nuYears = length(unique(year)))
+
+ggplot(monthlyData)+
+  geom_tile(aes(x = month_numeric, y = country, fill = nuYears))
+
+monthlyData <- DATA2 %>%
+  group_by(country,year) %>%
+  summarize(month_range = max(month_numeric) - min(month_numeric))
+            
+ggplot(monthlyData)+
+      geom_tile(aes(x = year, y = country, fill = month_range))+
+  scale_fill_viridis_c()
+            
+#seasonal biodiversity patterns
+            
 ggplot(DATA2, aes(x=month_numeric, y=spp_richness))+
   geom_point(aes(colour = year))+
   facet_wrap(~country)+
