30ac381ec3b3c400978bd9142c37b0e0fa75f06e
Diana Bowler <diana.bowler@idiv.de>
2021-08-17 10:16:53 +0200

    checking all tasks run

--- R/HPC_combine_site_trends.R
+++ R/HPC_combine_site_trends.R
@@ -12,7 +12,6 @@ countryTrends <- lapply(trendsFiles,function(x){
   
   #add on response from file name
   temp$Response <- strsplit(as.character(x),"__")[[1]][2]
-
   return(temp)
 
 })
@@ -20,3 +19,26 @@ countryTrends <- lapply(trendsFiles,function(x){
 countryTrends <- do.call(rbind,countryTrends)
 names(countryTrends)[which(names(countryTrends)=="siteID")] <- "site_id"
 saveRDS(countryTrends,file="outputs/stanTrends_site_level.rds")
+
+
+#check we have all data
+
+#get lists of tasks
+TaskID <- read.csv("outputs/ResponseTrends_TaskIDs.csv",as.is=T)
+sort(unique(TaskID$Response))
+
+#all responses?
+unique(countryTrends$Response)
+#we are missing alien_Abund, alien_SppRich, E10, EPT_Abund, EPT_SppRich,
+#insect_Abund, insect_SppRich
+
+missing <- c("alien_Abund", "alien_SppRich", "E10", "EPT_Abund","EPT_SppRich",
+             "insect_Abund", "insect_SppRich")
+
+#see which ones have some NAs
+temp <- subset(countryTrends, propNAs>0)
+table(temp$Response)
+
+missing2 <- names(table(temp$Response))
+
+allmissing <- sort(c(missing,missing2))
