65aea8a28836c262add7b1cf9dd0e95c8cb0b438
Ellen Welti <86353956+Ewelti@users.noreply.github.com>
2021-11-16 17:32:40 +0100

    Improving figures

--- plots/DensPlotsa_stan_2by4.R
+++ plots/DensPlotsa_stan_2by4.R
@@ -9,41 +9,11 @@ length(unique(response_stan$site_id))
 
 Ests <- read.csv("outputs/Yr_metaanaly_Ests.csv")
 
+tiff(filename = "plots/SlopeDistributions.tiff", width = 10, height = 6, units = 'in', res = 600, compression = 'lzw')
+
 par(mar=c(4,0.4,0.4,0.4), mfrow=c(1,2))
 
 #####################Taxonomic metrics ##############################
-#### Abundance #####
-abund <- subset(response_stan, Response == "abundance")
-abund <- abund$estimate[!is.na(abund$estimate)]
-percChange_perYr <- abund*100
-d <- density(percChange_perYr)
-a <- (max(d$y)+(max(d$y)/10)) *-3
-b <- (max(d$y)+(max(d$y)/10))*1
-plot(d, main="",ylab="",xlab="",cex.lab=2,xlim=c(-15,15),ylim=c(a,b),col="white",yaxt='n',xaxt='n')
-#axis(2, at=0, labels="Abun", las=1,cex.axis=1.3)
-##
-polygon(c(d$x[d$x >= 0 ], 0),
-        c(d$y[d$x >= 0 ], 0),
-        col = "cornflowerblue", border = "cornflowerblue", lwd =2)
-polygon(c(d$x[d$x <= 0 ], 0),
-        c(d$y[d$x <= 0 ], 0),
-        col = "tomato", border = "tomato", lwd =2)
-abund_Est <- subset(Ests, Response == "abundance")
-stand_abund <- lapply(abund_Est[,2:11],"*",100)
-yy <- (3.2/4*(b-a)+a)
-points(x=stand_abund$Estimate, y=yy, lwd=2,pch="|",cex=2)
-polygon(x=c(stand_abund$Q2.5, stand_abund$Q2.5, stand_abund$Q97.5, stand_abund$Q97.5),
-        y=c((yy-yy/18),(yy+yy/18),(yy+yy/18),(yy-yy/18)),
-        col = 1,border = 0,lwd = 1)
-polygon(x=c(stand_abund$Q5, stand_abund$Q5, stand_abund$Q95, stand_abund$Q95),
-        y=c((yy-yy/10),(yy+yy/10),(yy+yy/10),(yy-yy/10)),
-        col = 1,border = 0,lwd =1)
-polygon(x=c(stand_abund$Q10, stand_abund$Q10, stand_abund$Q90, stand_abund$Q90),
-        y=c((yy-yy/6),(yy+yy/6),(yy+yy/6),(yy-yy/6)),
-        col = 1,border = 0,lwd =1)
-legend(x=-18, y=(4/4*(b-a)+a), legend=("Abundance"), bty="n", cex=1.3)
-legend(x=5, y=(4/4*(b-a)+a), legend=expression(paste("+1.13% ",y^-1,, sep = "")), bty="n", cex=1.3)
-
 #### Spp Richness #####
 SppRich <- subset(response_stan, Response == "spp_richness")
 SppRich <- SppRich$estimate[!is.na(SppRich$estimate)]
@@ -51,11 +21,10 @@ SppRich <- SppRich$estimate[!is.na(SppRich$estimate)]
 ave_SppRich <- 27.28712314
 percChange_perYr<-(SppRich/ave_SppRich)*100
 d <- density(percChange_perYr)
-a <- (max(d$y)+(max(d$y)/10)) *-2
-b <- (max(d$y)+(max(d$y)/10))*2
-par(new=TRUE)
-plot(d, main="",ylab="",xlab="",cex.lab=2,xlim=c(-15,15),ylim=c(a,b),col="white",yaxt='n')
-title(xlab="Annual % change", line=2.4,cex.lab=1.3)
+a <- (max(d$y)+(max(d$y)/10)) *-3
+b <- (max(d$y)+(max(d$y)/10))*1
+plot(d, main="",ylab="",xlab="",cex.lab=2,xlim=c(-15,15),ylim=c(a,b),col="white",yaxt='n',yaxs="i")
+title(xlab=expression(paste("% change y"^"-1")), line=2.4,cex.lab=1.3)
 #axis(2, at=0, labels="SR", las=1,cex.axis=1.3)
 ##
 polygon(c(d$x[d$x >= 0 ], 0),
@@ -63,10 +32,10 @@ polygon(c(d$x[d$x >= 0 ], 0),
         col = "cornflowerblue", border = "cornflowerblue", lwd =2)
 polygon(c(d$x[d$x <= 0 ], 0),
         c(d$y[d$x <= 0 ], 0),
-        col = "tomato", border = "tomato", lwd =2)
+        col = "coral1", border = "coral1", lwd =2)
 SppRich_Est <- subset(Ests, Response == "spp_richness")
 stand_SppRich <- lapply(SppRich_Est[,2:11],"*",100/ave_SppRich)
-yy <- (2.2/4*(b-a)+a)
+yy <- (3.2/4*(b-a)+a)
 points(x=stand_SppRich$Estimate, y=yy, lwd=2,pch="|",cex=2)
 polygon(x=c(stand_SppRich$Q2.5, stand_SppRich$Q2.5, stand_SppRich$Q97.5, stand_SppRich$Q97.5),
         y=c((yy-yy/18),(yy+yy/18),(yy+yy/18),(yy-yy/18)),
@@ -77,8 +46,39 @@ polygon(x=c(stand_SppRich$Q5, stand_SppRich$Q5, stand_SppRich$Q95, stand_SppRich
 polygon(x=c(stand_SppRich$Q10, stand_SppRich$Q10, stand_SppRich$Q90, stand_SppRich$Q90),
         y=c((yy-yy/6),(yy+yy/6),(yy+yy/6),(yy-yy/6)),
         col = 1,border = 0,lwd = 1)
-legend(x=-18, y=(3/4*(b-a)+a), legend=("Species richness"), bty="n", cex=1.3)
-legend(x=5, y=(3/4*(b-a)+a), legend=expression(paste("+0.94% ",y^-1,, sep = "")), bty="n", cex=1.3)
+legend(x=-18, y=(4/4*(b-a)+a), legend=("a) Species richness"), bty="n", cex=1.3)
+legend(x=5, y=(4/4*(b-a)+a), legend=expression(paste("+0.94% ",y^-1,, sep = "")), bty="n", cex=1.3)
+
+#### Abundance #####
+abund <- subset(response_stan, Response == "abundance")
+abund <- abund$estimate[!is.na(abund$estimate)]
+percChange_perYr <- abund*100
+d <- density(percChange_perYr)
+a <- (max(d$y)+(max(d$y)/10)) *-2
+b <- (max(d$y)+(max(d$y)/10))*2
+par(new=TRUE)
+plot(d, main="",ylab="",xlab="",cex.lab=2,xlim=c(-15,15),ylim=c(a,b),col="white",yaxt='n',xaxt='n',yaxs="i")
+polygon(c(d$x[d$x >= 0 ], 0),
+        c(d$y[d$x >= 0 ], 0),
+        col = "cornflowerblue", border = "cornflowerblue", lwd =2)
+polygon(c(d$x[d$x <= 0 ], 0),
+        c(d$y[d$x <= 0 ], 0),
+        col = "coral1", border = "coral1", lwd =2)
+abund_Est <- subset(Ests, Response == "abundance")
+stand_abund <- lapply(abund_Est[,2:11],"*",100)
+yy <- (2.2/4*(b-a)+a)
+points(x=stand_abund$Estimate, y=yy, lwd=2,pch="|",cex=2)
+polygon(x=c(stand_abund$Q2.5, stand_abund$Q2.5, stand_abund$Q97.5, stand_abund$Q97.5),
+        y=c((yy-yy/18),(yy+yy/18),(yy+yy/18),(yy-yy/18)),
+        col = 1,border = 0,lwd = 1)
+polygon(x=c(stand_abund$Q5, stand_abund$Q5, stand_abund$Q95, stand_abund$Q95),
+        y=c((yy-yy/10),(yy+yy/10),(yy+yy/10),(yy-yy/10)),
+        col = 1,border = 0,lwd =1)
+polygon(x=c(stand_abund$Q10, stand_abund$Q10, stand_abund$Q90, stand_abund$Q90),
+        y=c((yy-yy/6),(yy+yy/6),(yy+yy/6),(yy-yy/6)),
+        col = 1,border = 0,lwd =1)
+legend(x=-18, y=(3/4*(b-a)+a), legend=("b) Abundance"), bty="n", cex=1.3)
+legend(x=5, y=(3/4*(b-a)+a), legend=expression(paste("+1.13% ",y^-1,, sep = "")), bty="n", cex=1.3)
 
 #### Shannon's Evenness #####
 E10 <- subset(response_stan, Response == "E10")
@@ -89,7 +89,7 @@ d <- density(percChange_perYr)
 a <- (max(d$y)+(max(d$y)/10))*-1
 b <- (max(d$y)+(max(d$y)/10))*3
 par(new=TRUE)
-plot(d, main="",ylab="",xlab="",cex.lab=2,xlim=c(-15,15),ylim=c(a,b),col="white",yaxt='n',xaxt='n')
+plot(d, main="",ylab="",xlab="",cex.lab=2,xlim=c(-15,15),ylim=c(a,b),col="white",yaxt='n',xaxt='n',yaxs="i")
 #axis(2, at=0, labels="Even", las=1,cex.axis=1.3)
 ##
 polygon(c(d$x[d$x >= 0 ], 0),
@@ -97,7 +97,7 @@ polygon(c(d$x[d$x >= 0 ], 0),
         col = "cornflowerblue", border = "cornflowerblue", lwd =2)
 polygon(c(d$x[d$x <= 0 ], 0),
         c(d$y[d$x <= 0 ], 0),
-        col = "tomato", border = "tomato", lwd =2)
+        col = "coral1", border = "coral1", lwd =2)
 E10_Est <- subset(Ests, Response == "E10")
 stand_E10 <- lapply(E10_Est[,2:11],"*",100)
 yy <- (1.2/4*(b-a)+a)
@@ -111,7 +111,7 @@ polygon(x=c(stand_E10$Q5, stand_E10$Q5, stand_E10$Q95, stand_E10$Q95),
 polygon(x=c(stand_E10$Q10, stand_E10$Q10, stand_E10$Q90, stand_E10$Q90),
         y=c((yy-yy/6),(yy+yy/6),(yy+yy/6),(yy-yy/6)),
         col = 1,border = 0,lwd =1)
-legend(x=-18, y=(2/4*(b-a)+a), legend=("Shannon's evenness"), bty="n", cex=1.3)
+legend(x=-18, y=(2/4*(b-a)+a), legend=("c) Evenness"), bty="n", cex=1.3)
 legend(x=5, y=(2/4*(b-a)+a), legend=expression(paste("-0.5% ",y^-1,, sep = "")), bty="n", cex=1.3)
 
 #### Turnover #####
@@ -125,7 +125,7 @@ d <- density(percChange_perYr)
 a <- (max(d$y)+(max(d$y)/10)) *0
 b <- (max(d$y)+(max(d$y)/10))*4
 par(new=TRUE)
-plot(d, main="",ylab="",xlab="",cex.lab=2,xlim=c(-15,15),ylim=c(a,b),col="white",yaxt='n',xaxt='n')
+plot(d, main="",ylab="",xlab="",cex.lab=2,xlim=c(-15,15),ylim=c(a,b),col="white",yaxt='n',xaxt='n',yaxs="i")
 #axis(2, at=0, labels="TurnO", las=1,cex.axis=1.3)
 ##
 polygon(c(d$x[d$x >= 0 ], 0),
@@ -133,7 +133,7 @@ polygon(c(d$x[d$x >= 0 ], 0),
         col = "cornflowerblue", border = "cornflowerblue", lwd =2)
 polygon(c(d$x[d$x <= 0 ], 0),
         c(d$y[d$x <= 0 ], 0),
-        col = "tomato", border = "tomato", lwd =2)
+        col = "coral1", border = "coral1", lwd =2)
 turnover_Est <- subset(Ests, Response == "turnover")
 stand_turnover <- lapply(turnover_Est[,2:11],"*",100/ave_turnover)
 yy <- (0.2/4*(b-a)+a)
@@ -147,71 +147,35 @@ polygon(x=c(stand_turnover$Q5, stand_turnover$Q5, stand_turnover$Q95, stand_turn
 polygon(x=c(stand_turnover$Q10, stand_turnover$Q10, stand_turnover$Q90, stand_turnover$Q90),
         y=c((yy-yy/6),(yy+yy/6),(yy+yy/6),(yy-yy/6)),
         col = 1,border = 0,lwd =1)
-legend(x=-18, y=(1/4*(b-a)+a), legend=("Turnover"), bty="n", cex=1.3)
+legend(x=-18, y=(1/4*(b-a)+a), legend=("d) Turnover"), bty="n", cex=1.3)
 legend(x=5, y=(1/4*(b-a)+a), legend=expression(paste("-0.3% ",y^-1,, sep = "")), bty="n", cex=1.3)
 
 ##
-box(lwd=2)
-abline(v=0, lwd=1, lty=2)
+box(lwd=2,col="white")
+axis(1,lwd=2)
+abline(v=0, lwd=1.5, lty=2)
 ##
 
 #####################Functional metrics ##############################
-#### Functional Redundancy #####
-FRed <- subset(response_stan, Response == "FRed")
-FRed <- FRed$estimate[!is.na(FRed$estimate)]
-#average Rao's Q = 0.291145846
-ave_FRed <- 0.291145846
-percChange_perYr<-(FRed/ave_FRed)*100
-d <- density(percChange_perYr)
-a <- (max(d$y)+(max(d$y)/10)) *-3
-b <- (max(d$y)+(max(d$y)/10))*1
-plot(d, main="",ylab="",xlab="",cex.lab=2,xlim=c(-15,15),ylim=c(a,b),col="white",yaxt='n',xaxt='n')
-#axis(2, at=0, labels="Rao's Q", las=1,cex.axis=1.3)
-##
-polygon(c(d$x[d$x >= 0 ], 0),
-        c(d$y[d$x >= 0 ], 0),
-        col = "cornflowerblue", border = "cornflowerblue", lwd =2)
-polygon(c(d$x[d$x <= 0 ], 0),
-        c(d$y[d$x <= 0 ], 0),
-        col = "tomato", border = "tomato", lwd =2)
-FRed_Est <- subset(Ests, Response == "FRed")
-stand_FRed <- lapply(FRed_Est[,2:11],"*",100/ave_FRed)
-yy <- (3.2/4*(b-a)+a)
-points(x=stand_FRed$Estimate, y=yy, lwd=2,pch="|",cex=2)
-polygon(x=c(stand_FRed$Q2.5, stand_FRed$Q2.5, stand_FRed$Q97.5, stand_FRed$Q97.5),
-        y=c((yy-yy/18),(yy+yy/18),(yy+yy/18),(yy-yy/18)),
-        col = 1,border = 0,lwd = 1)
-polygon(x=c(stand_FRed$Q5, stand_FRed$Q5, stand_FRed$Q95, stand_FRed$Q95),
-        y=c((yy-yy/10),(yy+yy/10),(yy+yy/10),(yy-yy/10)),
-        col = 1,border = 0,lwd =1)
-polygon(x=c(stand_FRed$Q10, stand_FRed$Q10, stand_FRed$Q90, stand_FRed$Q90),
-        y=c((yy-yy/6),(yy+yy/6),(yy+yy/6),(yy-yy/6)),
-        col = 1,border = 0,lwd =1)
-legend(x=-18, y=(4/4*(b-a)+a), legend=("Func. redundancy"), bty="n", cex=1.3)
-legend(x=5, y=(4/4*(b-a)+a), legend=expression(paste("+0.19% ",y^-1,, sep = "")), bty="n", cex=1.3)
-
 #### Functional Richness #####
 FRic <- subset(response_stan, Response == "FRic")
 FRic <- FRic$estimate[!is.na(FRic$estimate)]
 #average functional richness= 1372.61497
 percChange_perYr<-(FRic*100)
 d <- density(percChange_perYr)
-a <- (max(d$y)+(max(d$y)/10)) *-2
-b <- (max(d$y)+(max(d$y)/10))*2
-par(new=TRUE)
-plot(d, main="",ylab="",xlab="",cex.lab=2,xlim=c(-15,15),ylim=c(a,b),col="white",yaxt='n')
-title(xlab="Annual % change", line=2.4,cex.lab=1.3)
-#axis(2, at=0, labels="FRic", las=1,cex.axis=1.3)
-##
+a <- (max(d$y)+(max(d$y)/10)) *-3
+b <- (max(d$y)+(max(d$y)/10))*1
+plot(d, main="",ylab="",xlab="",cex.lab=2,xlim=c(-15,15),ylim=c(a,b),col="white",yaxt='n',yaxs="i")
+title(xlab=expression(paste("% change y"^"-1")), line=2.4,cex.lab=1.3)
 polygon(c(d$x[d$x >= 0 ], 0),
         c(d$y[d$x >= 0 ], 0),
         col = "cornflowerblue", border = "cornflowerblue", lwd =2)
 polygon(c(d$x[d$x <= 0 ], 0),
         c(d$y[d$x <= 0 ], 0),
-        col = "tomato", border = "tomato", lwd =2)
+        col = "coral1", border = "coral1", lwd =2)
 FRic_Est <- subset(Ests, Response == "func_rich")
 stand_FRic <- lapply(FRic_Est[,2:11],"*", 100)
-yy <- (2.2/4*(b-a)+a)
+yy <- (3.2/4*(b-a)+a)
 points(x=stand_FRic$Estimate, y=yy, lwd=2,pch="|",cex=2)
 polygon(x=c(stand_FRic$Q2.5, stand_FRic$Q2.5, stand_FRic$Q97.5, stand_FRic$Q97.5),
         y=c((yy-yy/18),(yy+yy/18),(yy+yy/18),(yy-yy/18)),
@@ -222,8 +186,40 @@ polygon(x=c(stand_FRic$Q5, stand_FRic$Q5, stand_FRic$Q95, stand_FRic$Q95),
 polygon(x=c(stand_FRic$Q10, stand_FRic$Q10, stand_FRic$Q90, stand_FRic$Q90),
         y=c((yy-yy/6),(yy+yy/6),(yy+yy/6),(yy-yy/6)),
         col = 1,border = 0,lwd =1)
-legend(x=-18, y=(3/4*(b-a)+a), legend=("Func. richness"), bty="n", cex=1.3)
-legend(x=5, y=(3/4*(b-a)+a), legend=expression(paste("+1% ",y^-1,, sep = "")), bty="n", cex=1.3)
+legend(x=-18, y=(4/4*(b-a)+a), legend=("e) Func. richness"), bty="n", cex=1.3)
+legend(x=5, y=(4/4*(b-a)+a), legend=expression(paste("+1% ",y^-1,, sep = "")), bty="n", cex=1.3)
+
+#### Functional Redundancy #####
+FRed <- subset(response_stan, Response == "FRed")
+FRed <- FRed$estimate[!is.na(FRed$estimate)]
+ave_FRed <- 0.291145846
+percChange_perYr<-(FRed/ave_FRed)*100
+d <- density(percChange_perYr)
+a <- (max(d$y)+(max(d$y)/10)) *-2
+b <- (max(d$y)+(max(d$y)/10))*2
+par(new=TRUE)
+plot(d, main="",ylab="",xlab="",cex.lab=2,xlim=c(-15,15),ylim=c(a,b),col="white",yaxt='n',xaxt='n',yaxs="i")
+polygon(c(d$x[d$x >= 0 ], 0),
+        c(d$y[d$x >= 0 ], 0),
+        col = "cornflowerblue", border = "cornflowerblue", lwd =2)
+polygon(c(d$x[d$x <= 0 ], 0),
+        c(d$y[d$x <= 0 ], 0),
+        col = "coral1", border = "coral1", lwd =2)
+FRed_Est <- subset(Ests, Response == "FRed")
+stand_FRed <- lapply(FRed_Est[,2:11],"*",100/ave_FRed)
+yy <- (2.2/4*(b-a)+a)
+points(x=stand_FRed$Estimate, y=yy, lwd=2,pch="|",cex=2)
+polygon(x=c(stand_FRed$Q2.5, stand_FRed$Q2.5, stand_FRed$Q97.5, stand_FRed$Q97.5),
+        y=c((yy-yy/18),(yy+yy/18),(yy+yy/18),(yy-yy/18)),
+        col = 1,border = 0,lwd = 1)
+polygon(x=c(stand_FRed$Q5, stand_FRed$Q5, stand_FRed$Q95, stand_FRed$Q95),
+        y=c((yy-yy/10),(yy+yy/10),(yy+yy/10),(yy-yy/10)),
+        col = 1,border = 0,lwd =1)
+polygon(x=c(stand_FRed$Q10, stand_FRed$Q10, stand_FRed$Q90, stand_FRed$Q90),
+        y=c((yy-yy/6),(yy+yy/6),(yy+yy/6),(yy-yy/6)),
+        col = 1,border = 0,lwd =1)
+legend(x=-18, y=(3/4*(b-a)+a), legend=("f) Func. redundancy"), bty="n", cex=1.3)
+legend(x=5, y=(3/4*(b-a)+a), legend=expression(paste("+0.19% ",y^-1,, sep = "")), bty="n", cex=1.3)
 
 #### Func evenness #####
 FEve <- subset(response_stan, Response == "FEve")
@@ -235,7 +231,7 @@ d <- density(percChange_perYr)
 a <- (max(d$y)+(max(d$y)/10)) *-1
 b <- (max(d$y)+(max(d$y)/10))*3
 par(new=TRUE)
-plot(d, main="",ylab="",xlab="",cex.lab=2,xlim=c(-15,15),ylim=c(a,b),col="white",yaxt='n',xaxt='n')
+plot(d, main="",ylab="",xlab="",cex.lab=2,xlim=c(-15,15),ylim=c(a,b),col="white",yaxt='n',xaxt='n',yaxs="i")
 #axis(2, at=0, labels="FEve", las=1,cex.axis=1.3)
 ##
 polygon(c(d$x[d$x >= 0 ], 0),
@@ -243,7 +239,7 @@ polygon(c(d$x[d$x >= 0 ], 0),
         col = "cornflowerblue", border = "cornflowerblue", lwd =2)
 polygon(c(d$x[d$x <= 0 ], 0),
         c(d$y[d$x <= 0 ], 0),
-        col = "tomato", border = "tomato", lwd =2)
+        col = "coral1", border = "coral1", lwd =2)
 FEve_Est <- subset(Ests, Response == "func_even")
 stand_FEve <- lapply(FEve_Est[,2:11],"*",100/ave_FEve)
 yy <- (1.2/4*(b-a)+a)
@@ -257,7 +253,7 @@ polygon(x=c(stand_FEve$Q5, stand_FEve$Q5, stand_FEve$Q95, stand_FEve$Q95),
 polygon(x=c(stand_FEve$Q10, stand_FEve$Q10, stand_FEve$Q90, stand_FEve$Q90),
         y=c((yy-yy/6),(yy+yy/6),(yy+yy/6),(yy-yy/6)),
         col = 1,border = 0,lwd =1)
-legend(x=-18, y=(2/4*(b-a)+a), legend=("Func. evenness"), bty="n", cex=1.3)
+legend(x=-18, y=(2/4*(b-a)+a), legend=("g) Func. evenness"), bty="n", cex=1.3)
 legend(x=5, y=(2/4*(b-a)+a), legend=expression(paste("-0.51% ",y^-1,, sep = "")), bty="n", cex=1.3)
 
 #### Func turnover #####
@@ -266,20 +262,17 @@ F_to <- F_to$estimate[!is.na(F_to$estimate)]
 #average Func turnover = 0.086496081
 #ave_F_to <- 0.086496081
 percChange_perYr <- ((exp(F_to)-1)*100)
-head(percChange_perYr2)
 d <- density(percChange_perYr)
 a <- (max(d$y)+(max(d$y)/10)) *0
 b <- (max(d$y)+(max(d$y)/10))*4
 par(new=TRUE)
-plot(d, main="",ylab="",xlab="",cex.lab=2,xlim=c(-15,15),ylim=c(a,b),col="white",yaxt='n',xaxt='n')
-#axis(2, at=0, labels="FTurno", las=1,cex.axis=1.3)
-##
+plot(d, main="",ylab="",xlab="",cex.lab=2,xlim=c(-15,15),ylim=c(a,b),col="white",yaxt='n',xaxt='n',yaxs="i")
 polygon(c(d$x[d$x >= 0 ], 0),
         c(d$y[d$x >= 0 ], 0),
-        col = "cornflowerblue", border = "cornflowerblue", lwd =2)
+        col = "cornflowerblue", border = "cornflowerblue", lwd =3)
 polygon(c(d$x[d$x <= 0 ], 0),
         c(d$y[d$x <= 0 ], 0),
-        col = "tomato", border = "tomato", lwd =2)
+        col = "coral1", border = "coral1", lwd =2)
 F_to_Est <- subset(Ests, Response == "func_turnover")
 stand_F_to <- (exp(F_to_Est[,2:11])-1)*100
 yy <- (0.2/4*(b-a)+a)
@@ -293,11 +286,14 @@ polygon(x=c(stand_F_to$Q5, stand_F_to$Q5, stand_F_to$Q95, stand_F_to$Q95),
 polygon(x=c(stand_F_to$Q10, stand_F_to$Q10, stand_F_to$Q90, stand_F_to$Q90),
         y=c((yy-yy/6),(yy+yy/6),(yy+yy/6),(yy-yy/6)),
         col = 1,border = 0,lwd =1)
-legend(x=-18, y=(1/4*(b-a)+a), legend=("Func. turnover"), bty="n", cex=1.3)
+legend(x=-18, y=(1/4*(b-a)+a), legend=("h) Func. turnover"), bty="n", cex=1.3)
 legend(x=5, y=(1/4*(b-a)+a), legend=expression(paste("-1.41% ",y^-1,, sep = "")), bty="n", cex=1.3)
 
 ##
-box(lwd=2)
-abline(v=0, lwd=1, lty=2)
+box(lwd=2,col="white")
+axis(1,lwd=2)
+abline(v=0, lwd=1.5, lty=2)
 ##
+
+dev.off()
 ########################################
--- plots/MovingAverage.R
+++ plots/MovingAverage.R
@@ -6,55 +6,105 @@ MA <- read.csv("outputs/movingAve_YrEsts.csv")
 MA$meanYr <- (MA$StartYear+ 4.5)
 head(MA)
 
-tiff(filename = "plots/MovingAverages.tiff", width = 7, height = 6, units = 'in', res = 600, compression = 'lzw')
-par(mfrow=c(2,1),mar=c(2,3,0.2,0.2))
+tiff(filename = "plots/MovingAverages.tiff", width = 8, height = 6.5, units = 'in', res = 600, compression = 'lzw')
+par(mfrow=c(2,2),mar=c(2,4,0.2,0.2))
 
 #plot for spp richness
 SR <- subset(MA, Response == "spp_richness")
 ave_SppRich <- 27.28712314
-st <-(SR[1:44,3:12]/ave_SppRich)*100
+st <-(SR[1:(nrow(SR)),3:12]/ave_SppRich)*100
 sr <- cbind(SR$StartYear, SR$site_num, SR$meanYr, st)
 names(sr)[names(sr) == 'SR$StartYear'] <- 'StartYear'
 names(sr)[names(sr) == 'SR$site_num'] <- 'site_num'
 names(sr)[names(sr) == 'SR$meanYr'] <- 'meanYr'
 #select yrs with enough sites to be representative
-SRs <- sr[ which(sr$site_num >=100), ]
-#SRs <- sr[ which(sr$StartYear >=1989), ] # this is min 7 countries
+#SRs <- sr[ which(sr$site_num >=100), ]
+SRs <- sr[ which(sr$StartYear >1989), ] # this is min 7 countries
 #SRs <- sr[ which(sr$StartYear >=1980), ] #this is min 5 countries
 
-plot(SRs$Estimate~SRs$meanYr,ylab="", cex=1.5, xlab="", type="n", las=1, ylim=c(-2.1,5.5), xlim=c(1990,2018))#ylim=c(-0.8,2.55))
-title(ylab="Species richness (% change)", line=2,cex.lab=1)
+plot(SRs$Estimate~SRs$meanYr,ylab="", cex=1.5, xlab="", type="n", las=1, ylim=c(-1.8,4.1), xlim=c(1994.5,2018.5))#ylim=c(-0.8,2.55))
+title(ylab=expression(paste("Species richness (% y"^"-1", ")")), line=2,cex.lab=1.4)
 #title(xlab="Mean year of moving window", line=2,cex.lab=1.3)
-polygon(x = c(0, 0, 2040, 2040), y = c(-10, 0, 0, -10), col ="brown3", border = NA)
+polygon(x = c(0, 0, 2040, 2040), y = c(-100, 0, 0, -100), col ="coral1", border = NA)
+polygon(x = c(0, 0, 2040, 2040), y = c(100, 0, 0, 100), col ="lightsteelblue1", border = NA)
 abline(h=0,lty=2)
 polygon(x = c(SRs$meanYr,rev(SRs$meanYr)), y = c(SRs$Q2.5,rev(SRs$Q97.5)),col="#00000020", border = NA)
-polygon(x = c(SRs$meanYr,rev(SRs$meanYr)), y = c(SRs$Q5,rev(SRs$Q95)), col ="#00000027", border = NA)
-polygon(x = c(SRs$meanYr,rev(SRs$meanYr)), y = c(SRs$Q10,rev(SRs$Q90)), col ="#00000027", border = NA)
-box(lwd=2)
+polygon(x = c(SRs$meanYr,rev(SRs$meanYr)), y = c(SRs$Q5,rev(SRs$Q95)), col ="#00000020", border = NA)
+polygon(x = c(SRs$meanYr,rev(SRs$meanYr)), y = c(SRs$Q10,rev(SRs$Q90)), col ="#00000020", border = NA)
+#box(lwd=2)
 points(SRs$Estimate~SRs$meanYr, pch=21,bg=1,cex=1.8)#pch="-",
 points(SRs$Estimate~SRs$meanYr,type="l",lwd=2)
+legend("topright", bty="n", legend="a",cex=1.5)
 
 #plot for abundance
 SR <- subset(MA, Response == "abundance")
-st <-(SR[1:44,3:12]*100)
+st <-(SR[1:(nrow(SR)),3:12]*100)
 sr <- cbind(SR$StartYear, SR$site_num, SR$meanYr, st)
 names(sr)[names(sr) == 'SR$StartYear'] <- 'StartYear'
 names(sr)[names(sr) == 'SR$site_num'] <- 'site_num'
 names(sr)[names(sr) == 'SR$meanYr'] <- 'meanYr'
 #select yrs with enough sites to be representative
-SRs <- sr[ which(sr$site_num >=100), ]
+SRs <- sr[ which(sr$StartYear >1989), ] # this is min 7 countries
 
-plot(SRs$Estimate~SRs$meanYr,ylab="", cex=1.5, xlab="", type="n", las=1, ylim=c(-11.5,19.5), xlim=c(1990,2018)) #ylim=c(-0.57,0.4))
-title(ylab="Abundance (% change)", line=2,cex.lab=1)
+plot(SRs$Estimate~SRs$meanYr,ylab="", cex=1.5, xlab="", type="n", las=1, ylim=c(-5.5,5.3), xlim=c(1994.5,2018.5)) #ylim=c(-0.57,0.4))
+title(ylab=expression(paste("Abundance (% y"^"-1", ")")), line=2,cex.lab=1.4)
 title(xlab="Mean year of moving window", line=2.4,cex.lab=1.3)
-polygon(x = c(0, 0, 2040, 2040), y = c(-100, 0, 0, -100), col ="brown3", border = NA)
+polygon(x = c(0, 0, 2040, 2040), y = c(-100, 0, 0, -100), col ="coral1", border = NA)
+polygon(x = c(0, 0, 2040, 2040), y = c(100, 0, 0, 100), col ="lightsteelblue1", border = NA)
 abline(h=0,lty=2)
 polygon(x = c(SRs$meanYr,rev(SRs$meanYr)), y = c(SRs$Q2.5,rev(SRs$Q97.5)),col="#00000020", border = NA)
-polygon(x = c(SRs$meanYr,rev(SRs$meanYr)), y = c(SRs$Q5,rev(SRs$Q95)), col ="#00000027", border = NA)
-polygon(x = c(SRs$meanYr,rev(SRs$meanYr)), y = c(SRs$Q10,rev(SRs$Q90)), col ="#00000027", border = NA)
-box(lwd=2)
+polygon(x = c(SRs$meanYr,rev(SRs$meanYr)), y = c(SRs$Q5,rev(SRs$Q95)), col ="#00000020", border = NA)
+polygon(x = c(SRs$meanYr,rev(SRs$meanYr)), y = c(SRs$Q10,rev(SRs$Q90)), col ="#00000020", border = NA)
 points(SRs$Estimate~SRs$meanYr, pch=21,bg=1,cex=1.8)#pch="-",
 points(SRs$Estimate~SRs$meanYr,type="l",lwd=2)
+legend("topright", bty="n", legend="b",cex=1.5)
+
+#plot for FRic
+SR <- subset(MA, Response == "FRic")
+st <-(SR[1:(nrow(SR)),3:12])*100
+sr <- cbind(SR$StartYear, SR$site_num, SR$meanYr, st)
+names(sr)[names(sr) == 'SR$StartYear'] <- 'StartYear'
+names(sr)[names(sr) == 'SR$site_num'] <- 'site_num'
+names(sr)[names(sr) == 'SR$meanYr'] <- 'meanYr'
+#select yrs with enough sites to be representative
+SRs <- sr[ which(sr$StartYear >=1989), ] # this is min 7 countries
+
+plot(SRs$Estimate~SRs$meanYr,ylab="", cex=1.5, xlab="", type="n", las=1, ylim=c(-8,6), xlim=c(1994.5,2018.5)) #ylim=c(-0.57,0.4))
+title(ylab=expression(paste("Functional richness (% y"^"-1", ")")), line=2,cex.lab=1.4)
+title(xlab="Mean year of moving window", line=2.4,cex.lab=1.3)
+polygon(x = c(0, 0, 2040, 2040), y = c(-100, 0, 0, -100), col ="coral1", border = NA)
+polygon(x = c(0, 0, 2040, 2040), y = c(100, 0, 0, 100), col ="lightsteelblue1", border = NA)
+abline(h=0,lty=2)
+polygon(x = c(SRs$meanYr,rev(SRs$meanYr)), y = c(SRs$Q2.5,rev(SRs$Q97.5)),col="#00000020", border = NA)
+polygon(x = c(SRs$meanYr,rev(SRs$meanYr)), y = c(SRs$Q5,rev(SRs$Q95)), col ="#00000020", border = NA)
+polygon(x = c(SRs$meanYr,rev(SRs$meanYr)), y = c(SRs$Q10,rev(SRs$Q90)), col ="#00000020", border = NA)
+points(SRs$Estimate~SRs$meanYr, pch=21,bg=1,cex=1.8)#pch="-",
+points(SRs$Estimate~SRs$meanYr,type="l",lwd=2)
+legend("topright", bty="n", legend="c",cex=1.5)
+
+#plot for FRed
+SR <- subset(MA, Response == "FRed")
+ave_FRed <- 0.291145846
+st <-(SR[1:(nrow(SR)),3:12]/ave_FRed)*100
+sr <- cbind(SR$StartYear, SR$site_num, SR$meanYr, st)
+names(sr)[names(sr) == 'SR$StartYear'] <- 'StartYear'
+names(sr)[names(sr) == 'SR$site_num'] <- 'site_num'
+names(sr)[names(sr) == 'SR$meanYr'] <- 'meanYr'
+#select yrs with enough sites to be representative
+SRs <- sr[ which(sr$StartYear >=1989), ] # this is min 7 countries
+
+plot(SRs$Estimate~SRs$meanYr,ylab="", cex=1.5, xlab="", type="n", las=1, ylim=c(-5.5,1.7), xlim=c(1994.5,2018.5)) #ylim=c(-0.57,0.4))
+title(ylab=expression(paste("Functional redundancy (% y"^"-1", ")")), line=2,cex.lab=1.4)
+title(xlab="Mean year of moving window", line=2.4,cex.lab=1.3)
+polygon(x = c(0, 0, 2040, 2040), y = c(-100, 0, 0, -100), col ="coral1", border = NA)
+polygon(x = c(0, 0, 2040, 2040), y = c(100, 0, 0, 100), col ="lightsteelblue1", border = NA)
+abline(h=0,lty=2)
+polygon(x = c(SRs$meanYr,rev(SRs$meanYr)), y = c(SRs$Q2.5,rev(SRs$Q97.5)),col="#00000020", border = NA)
+polygon(x = c(SRs$meanYr,rev(SRs$meanYr)), y = c(SRs$Q5,rev(SRs$Q95)), col ="#00000020", border = NA)
+polygon(x = c(SRs$meanYr,rev(SRs$meanYr)), y = c(SRs$Q10,rev(SRs$Q90)), col ="#00000020", border = NA)
+points(SRs$Estimate~SRs$meanYr, pch=21,bg=1,cex=1.8)#pch="-",
+points(SRs$Estimate~SRs$meanYr,type="l",lwd=2)
+legend("topright", bty="n", legend="d",cex=1.5)
 
 dev.off()
 
