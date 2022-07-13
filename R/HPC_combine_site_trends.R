0aca2c5317927db6cc2fcd4c5f8ab52132f34784
mischiefmao@gmail.com <mischiefmao@gmail.com>
2022-07-13 16:45:52 -0600

    making one file of all high threshold moving average estimates

--- R/HPC_combine_site_trends.R
+++ R/HPC_combine_site_trends.R
@@ -228,6 +228,60 @@ head(MoAv6)
 ##
 write.csv(MoAv6, "outputs/movingAve_YrEsts.csv")
 
+##############################################
+
+### high threshold moving average yr syntheses! #####
+setwd("C:/Users/Ellen/Desktop/aquatic_data/git/EuroAquaticMacroInverts/outputs/MA_higherthreshold_meta")
+path <- "C:/Users/Ellen/Desktop/aquatic_data/git/EuroAquaticMacroInverts/outputs/MA_higherthreshold_meta"
+
+require(data.table)
+library(brms)
+
+files = list.files(path = path, pattern = '\\.rds$')
+
+dat_list = lapply(files, function(x){
+  fit <- readRDS(x)
+  fixed_995 <- fixef(fit, probs = c(0.005, 0.995))
+  fixed_995 <- fixef(fit, probs = c(0.005, 0.995))
+  fixed_975 <- fixef(fit, probs = c(0.025, 0.975))
+  fixed_95 <- fixef(fit, probs = c(0.05, 0.95))
+  fixed_90 <- fixef(fit, probs = c(0.1, 0.9))
+  nam <- gsub("metaanalysis_movingaverage_","", x)
+  name <- gsub(".rds","", nam)
+  name <- gsub("_1","__1", name)
+  name <- gsub("_2","__2", name)
+  response <- strsplit(as.character(name),"__")[[1]][1]
+  year <- strsplit(as.character(name),"__")[[1]][2]
+  fixed <- list(Response=response, StartYear=year, fixed_995[,1:4], fixed_975[,3:4],
+                fixed_95[,3:4],fixed_90[,3:4])
+  fixed <-data.frame(lapply(fixed, function(x) t(data.frame(x))))
+  return(fixed)
+})
+
+MovAve <- do.call(rbind.data.frame, dat_list)
+head(MovAve)
+unique(MovAve$Response)
+##
+setwd("C:/Users/Ellen/Desktop/aquatic_data/git/EuroAquaticMacroInverts/")
+
+htma <- readRDS("outputs/stanTrends_site_level_movingaveragesHTMV.rds")
+head(htma)
+library(data.table)
+
+subs <- subset(htma, htma$Response == "abundance")
+DT <- data.table(subs)
+sitecount_ab <- DT[, .(site_num = length(unique(site_id))), by = StartYear]
+sitecount_ab
+
+MoAv1 <- merge(MovAve,sitecount_ab,by="StartYear", all=T)
+head(MoAv1)
+##
+write.csv(MoAv1, "outputs/HighThresholdMovingAve_YrEsts.csv")
+
+##
+
+##############################################
+
 ### moving average yr syntheses from splits by latitude! #####
 setwd("C:/Users/ewelti/Desktop/git/EuroAquaticMacroInverts/outputs/movingaverage_meta_split")
 path <- "C:/Users/ewelti/Desktop/git/EuroAquaticMacroInverts/outputs/movingaverage_meta_split"
