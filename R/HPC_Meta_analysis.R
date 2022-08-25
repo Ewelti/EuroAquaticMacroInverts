0f959c43b436663fe6bb1eaef95454b6b1a55140
Diana Bowler <diana.e.bowler@gmail.com>
2022-08-25 08:20:32 +0100

    update with se function

--- R/HPC_Meta_analysis.R
+++ R/HPC_Meta_analysis.R
@@ -42,7 +42,7 @@ options(mc.cores = cpus_per_task)
 
 #fit weighted model
 
-fit1 <- brm(estimate|weights(w) ~ 1 + (1|study_id) + (1|country),
+fit1 <- brm(estimate|se(sd) ~ 1 + (1|study_id) + (1|country),
             data = response_stan, iter=5000, inits = 0,
             chains = 4, prior = prior1,
             control = list(adapt_delta = 0.90,
@@ -52,14 +52,14 @@ fit1 <- brm(estimate|weights(w) ~ 1 + (1|study_id) + (1|country),
 
 saveRDS(fit1,file=paste0("metaanalysis_",myResponse,".rds"))
 
-# unweighted model 
-
-fit1 <- brm(estimate ~ 1 + (1|study_id) + (1|country),
-            data = response_stan, iter=5000, 
-            chains = 4, prior = prior1,
-            control = list(adapt_delta = 0.90, 
-                           max_treedepth = 12))
-
-### save output ####
-
-saveRDS(fit1,file=paste0("metaanalysis_unweighted_",myResponse,".rds"))
+# # unweighted model 
+# 
+# fit1 <- brm(estimate ~ 1 + (1|study_id) + (1|country),
+#             data = response_stan, iter=5000, 
+#             chains = 4, prior = prior1,
+#             control = list(adapt_delta = 0.90, 
+#                            max_treedepth = 12))
+# 
+# ### save output ####
+# 
+# saveRDS(fit1,file=paste0("metaanalysis_unweighted_",myResponse,".rds"))
